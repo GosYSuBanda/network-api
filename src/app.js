d1c17389948d8340c6cd3b06a5e87424fd5d8844
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-06-29 14:45:18 -0500

    CORS

--- src/app.js
+++ src/app.js
@@ -22,7 +22,10 @@ const app = express();
 connectDB();
 
 // Middleware global
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
